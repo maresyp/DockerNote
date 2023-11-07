781f1887b61f4bf507054be7b93b5795f02f636d
maresyp <mattq.2000@icloud.com>
2023-11-07 14:43:48 +0100

    working version of file transfer between api

--- balancer/src/main.py
+++ balancer/src/main.py
@@ -1,7 +1,20 @@
+from pathlib import Path
+
+import aiofiles
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
+    await run_notebook()
     return {"Hello": "World"}
+
+async def run_notebook():
+    client = httpx.AsyncClient()
+    nb_path = Path('app/my_notebook.ipynb')
+    async with aiofiles.open(nb_path, 'rb') as f:
+        file = {'notebook': ('my_notebook.ipynb', await f.read(), 'application/octet-stream')}
+        response = await client.post("http://worker:8312/run_jupyter_notebook", files=file)
+        print(f'{response=}')
--- dockernote_worker/src/api/worker_endpoint.py
+++ dockernote_worker/src/api/worker_endpoint.py
@@ -1,22 +1,35 @@
+from __future__ import annotations
+
+import shutil
 import subprocess
 
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 
 
 class WorkerEndpoint:
     def __init__(self, *args, **kwargs):
         self.router = APIRouter()
-        self.router.add_api_route('/run_jupyter_notebook', self.run_jupyter_notebook, methods=['GET'])
+        self.router.add_api_route('/run_jupyter_notebook', self.run_jupyter_notebook, methods=['POST'])
 
     @staticmethod
-    def run_jupyter_notebook(notebook):
-        cmd = ['jupyter', 'nbconvert', '--execute', '--to', 'notebook', notebook]
+    def run_jupyter_notebook(notebook: UploadFile = File(...)): # noqa: B008
+        try:
+            with open(notebook.filename, 'wb') as f:
+                shutil.copyfileobj(notebook.file, f)
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            notebook.file.close()
+
+        cmd = ['jupyter', 'nbconvert', '--execute', '--to', 'notebook', notebook.filename]
         result = subprocess.run(cmd, stdout=subprocess.PIPE, timeout=3600)
         if result.returncode != 0:
             return {
                 'status': 'error',
+                'file': notebook.filename,
             }
         return {
             'status': 'ok',
+            'file': notebook.filename,
             'result': result.stdout.decode('utf-8'),
         }
