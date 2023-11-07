abee441b38b271d037b4c211ca30bc9d0c0b0486
maresyp <mattq.2000@icloud.com>
2023-11-07 15:51:57 +0100

    improve run_jupyter_notebook

--- dockernote_worker/src/api/worker_endpoint.py
+++ dockernote_worker/src/api/worker_endpoint.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import shutil
 import subprocess
+import tempfile
+from pathlib import Path
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, HTTPException, UploadFile
+from fastapi.responses import FileResponse
 
 
 class WorkerEndpoint:
@@ -12,24 +15,27 @@ class WorkerEndpoint:
         self.router.add_api_route('/run_jupyter_notebook', self.run_jupyter_notebook, methods=['POST'])
 
     @staticmethod
-    def run_jupyter_notebook(notebook: UploadFile = File(...)): # noqa: B008
-        try:
-            with open(notebook.filename, 'wb') as f:
-                shutil.copyfileobj(notebook.file, f)
-        except Exception:
-            return {"message": "There was an error uploading the file"}
-        finally:
-            notebook.file.close()
+    def run_jupyter_notebook(notebook: UploadFile = File(...)) -> FileResponse: # noqa: B008
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            temporary_notebook: Path = Path(tmpdirname) / notebook.filename
+            try:
+                with Path.open(temporary_notebook, 'wb') as f:
+                    shutil.copyfileobj(notebook.file, f)
+            except OSError as err:
+                raise HTTPException(status_code=500, detail={
+                    'status': 'error',
+                    'file': notebook.filename,
+                }) from err
+            finally:
+                notebook.file.close()
 
-        cmd = ['jupyter', 'nbconvert', '--execute', '--to', 'notebook', notebook.filename]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, timeout=3600)
-        if result.returncode != 0:
-            return {
+            cmd = ['jupyter', 'nbconvert', '--inplace', '--execute', '--to', 'notebook', str(temporary_notebook)]
+            result = subprocess.run(cmd, stdout=subprocess.PIPE, timeout=3600)
+
+            if result.returncode != 0:
+                raise HTTPException(status_code=500, detail={
                 'status': 'error',
                 'file': notebook.filename,
-            }
-        return {
-            'status': 'ok',
-            'file': notebook.filename,
-            'result': result.stdout.decode('utf-8'),
-        }
+            })
+
+            return FileResponse(temporary_notebook)
